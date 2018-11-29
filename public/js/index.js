4d29236e53baf96e448bb09b00e3b8131a33fa3a
Robert Mugattarov <mugattarov@gmail.com>
2018-11-29 16:11:28 +0300

    Updates

--- public/js/index.js
+++ public/js/index.js
@@ -3,15 +3,27 @@ var socket = io();
 socket.on('connect', function () {
   console.log('Connected to server');
 
-  socket.on('newMessage', function (newMsg) {
-    console.log('newMessage', newMsg);
-  });
-});
+  socket.on('newMessage', function (message) {
+    console.log('newMessage', message);
 
-socket.on('newMessage', function (newMsg) {
-  console.log('newMessage', newMsg);
+    var li = jQuery('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+
+    jQuery('#messages').append(li);
+  });
 });
 
 socket.on('disconnect', function () {
   console.log('Disconnected from server');
+});
+
+jQuery('#message-form').on('submit', function (e) {
+  e.preventDefault();
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text: jQuery('[name=message]').val()
+  }, function () {
+
+  });
 });
--- server/server.js
+++ server/server.js
@@ -19,10 +19,11 @@ io.on('connection', (socket) => {
   socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app!'));
   socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined!'));
 
-  socket.on('createMessage', (message) => {
+  socket.on('createMessage', (message, callback) => {
     console.log('createMessage', message);
 
-    socket.broadcast.emit('newMessage', generateMessage(message.from, message.text));
+    io.emit('newMessage', generateMessage(message.from, message.text));
+    callback('From the server');
   });
 
   socket.on('disconnect', (socket) => {
