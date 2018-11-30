f19055879a921ed44237b7a55d62baada2e99cfc
Robert Mugattarov <mugattarov@gmail.com>
2018-11-30 12:59:00 +0300

    Updates

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ io.on('connection', (socket) => {
     console.log('createMessage', message);
 
     io.emit('newMessage', generateMessage(message.from, message.text));
-    callback('From the server');
+    callback();
   });
 
   socket.on('createLocationMessage', (coords) => {
